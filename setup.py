98cedbeb00a990e9a9a920cc11efa89fd202ffb8
Bradley Cicenas <bradley.cicenas@gmail.com>
2016-03-10 13:58:22 -0500

    add setup.cfg, update package name

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 
 exec(open('haproxytop/version.py').read())
 
-setup(name='haproxytop',
+setup(name='haproxy-top',
       version=version,
       packages=['haproxytop'],
       description='Commandline utility for monitoring HAProxy',
       author='Bradley Cicenas',
       author_email='bradley.cicenas@gmail.com',
-      url='https://github.com/bcicen/haproxytop',
+      url='https://github.com/bcicen/haproxy-top',
       install_requires=['haproxy-stats>=1.2'],
       license='http://opensource.org/licenses/MIT',
       classifiers=(
